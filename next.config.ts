5e1cf47ffdf55835ad85c0b6c819a986ab8805f1
Shadrack Moses <shaddyperez09@gmail.com>
2026-05-21 11:21:04 +0300

    Remove conflicting domain redirect

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [
-          {
-            type: "host",
-            value: "www.wincatravels.co.ke",
-          },
-        ],
-        destination: "https://wincatravels.co.ke/:path*",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
